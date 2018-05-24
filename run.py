70e02117e78448fa26970114fad8b956ad002725
ShivSoni5 <shivsonic05@gmail.com>
2018-05-24 23:15:05 +0530

    indentation error removed for python3

--- run.py
+++ run.py
@@ -12,7 +12,7 @@ import webbrowser as wb
 
 BOLD = "\033[1m"   #use to bold the text
 END = "\033[0m"    #use to close the bold text
-CLIENT_ACCESS_TOKEN = "ddc70981eb8e4288aabeb842a40563ed"
+CLIENT_ACCESS_TOKEN = "2245d4ab7c99466e806c8986a18234c4"
 ai = apiai.ApiAI(CLIENT_ACCESS_TOKEN)
 
 google_search = "https://www.google.com/search?q="
@@ -21,49 +21,48 @@ google_drive = "https://drive.google.com"
 gmail = "https://mail.google.com"
 
 try:
-	r = sr.Recognizer()
-	with sr.Microphone() as source:
-		system("clear")
-        	print color(BOLD+"Hola!\nAsk me anything."+END,"green")
-		while True:        
-			audio = r.listen(source)
-
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        system("clear")
+        print(color(BOLD+"Hola!\nAsk me anything."+END,"green"))
+        while True:        
+            audio = r.listen(source)
 
 #	while True:	
-			try:
-				query = r.recognize_google(audio)
-			
-				print color("                               "+BOLD+query+END+"\n","red")
-				request = ai.text_request()
-				request.query = query  
-				response = request.getresponse()
-				json_data = (response.read())
-    				say =  json.loads(json_data)
-				speech = say['result']['fulfillment']['speech']
-				search = speech.split(":")
-				if search[0] == "Google" or search[0] == "Google and Google":
-					wb.open_new_tab(google_search+search[1])
-					print()
-				elif search[0] == "Wiki" :
-					try:
-						wiki_say = wiki.summary(search[1])
-						print color(BOLD+wiki_say+END+"\n","green")
-					except wiki.exceptions.DisambiguationError:
-						print color(BOLD+"Try to google it because it is very confusing for me"+END,"red")
-				elif search[0] == "Youtube":
-					wb.open_new_tab(youtube_search+search[1])
-					print("")
-				elif search[0] == "Drive":
-					wb.open_new_tab(google_drive)
-					print("")
-				elif search[0] == "Gmail":
-					print("")
-					wb.open_new_tab(gmail)
-				else :
-					print color(BOLD+speech+END+"\n","green")
+            try:
+                query = r.recognize_google(audio)
+    			
+                print(color("                               "+BOLD+query+END+"\n","red"))
+                request = ai.text_request()
+                request.query = query  
+                response = request.getresponse()
+                json_data = (response.read())
+                say =  json.loads(json_data)
+                speech = say['result']['fulfillment']['speech']
+                search = speech.split(":")
+                if search[0] == "Google" or search[0] == "Google and Google":
+                    wb.open_new_tab(google_search+search[1])
+                    print()
+                elif search[0] == "Wiki" :
+                    try:
+                        wiki_say = wiki.summary(search[1])
+                        print(color(BOLD+wiki_say+END+"\n","green"))
+                    except wiki.exceptions.DisambiguationError:
+                        print(color(BOLD+"Try to google it because it is very confusing for me"+END,"red"))
+                elif search[0] == "Youtube":
+                    wb.open_new_tab(youtube_search+search[1])
+                    print("")
+                elif search[0] == "Drive":
+                    wb.open_new_tab(google_drive)
+                    print("")
+                elif search[0] == "Gmail":
+                    print("")
+                    wb.open_new_tab(gmail)
+                else :
+                    print (color(BOLD+speech+END+"\n","green"))
 				
-			except sr.UnknownValueError:
-				print color("Listening","blue")
+            except sr.UnknownValueError:
+                print (color("Listening","blue"))
 
 except KeyboardInterrupt:
-	print color(BOLD+" Bye!"+END, "cyan")
+    print (color(BOLD+" Bye!"+END, "cyan"))
